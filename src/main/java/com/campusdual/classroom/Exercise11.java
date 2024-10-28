e868c77517c5b1aef2054698717a23a4079eea93
Ismael Leon Cal <ileoncal@gmail.com>
2024-10-28 13:13:16 +0100

    Ejercicio 11 Done

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,20 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        blackRemote.turnOn();//Encender tele
+        System.out.println("TV encendida con el mando color " + blackRemote.getColor());
+        blackRemote.channelUp();
+        System.out.println("Se ha subido un canal");
+        blackRemote.channelDown();
+        System.out.println("Se ha bajado un canal");
+        blackRemote.volumeUp();
+        System.out.println("Se ha subido el volumen");
+        blackRemote.volumeDown();
+        System.out.println("Se ha bajado el volumen");
+        blackRemote.turnOff();
+        System.out.println("TV encendida con el mando color " + blackRemote.getColor());
+
     }
 
 }
